0d15acc466a4c74ffbe0849467d5afe875199792
GiyoCode <uzuegbunamfranklin2@gmail.com>
2026-04-24 02:25:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -699,8 +699,8 @@ def lock_weekly_rf_if_needed():
         siphoned_cash += siphon_amount
 
         effective_balance = real_balance 
-
-        weekly_rf = 10
+        weekly_rf = round(effective_balance * RF_PERCENT, 6)
+        # weekly_rf = 10
 
         logger.info("===========================================")
         logger.info(f"NEW WEEK LOCKED: {current_week}")
